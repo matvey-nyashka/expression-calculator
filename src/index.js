dcbc0206f15e831a9265b21ff637513a8f6df6be
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-08-31 00:30:05 +0300

    feat: added solution of task

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ function eval() {
     return;
 }
 
+function fer(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "/") {
+          if (array[i+1] === "0") {
+            throw new Error("TypeError: Division by zero.");
+          }
+        }
+      }
+    let leftPair = 0;
+    let rightPair = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === "(") {
+        leftPair++;
+      }
+      if (array[i] === ")") {
+        rightPair++;
+      }
+    }
+    if (leftPair != rightPair) {
+      throw new Error("ExpressionError: Brackets must be paired.");
+    }
+    
+  }
+
 function expressionCalculator(expr) {
-    // write your solution here
+    let arr = expr.replace(/\s/g,"").split("");
+    fer(arr);
+    result = new Function("return " + expr.replace(/\s/g,""));
+    return result();
 }
 
 module.exports = {
